c919946b75182a21248e60f324cf7bc2616ba0d2
Aelous-6 <616521549@qq.com>
2015-12-30 00:19:04 +0800

    添加欢迎页面

--- app/src/main/java/com/hufu/weatherman/activity/MainActivity.java
+++ app/src/main/java/com/hufu/weatherman/activity/MainActivity.java
@@ -1,40 +1,28 @@
-package com.hufu.weatherman;
+package com.hufu.weatherman.activity;
 
 import android.annotation.TargetApi;
-import android.app.ActivityManager;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.os.Build;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 
-import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.hufu.weatherman.R;
 import com.hufu.weatherman.fragment.ForecastFragment;
 import com.hufu.weatherman.fragment.LifeFragment;
 import com.hufu.weatherman.fragment.LiveFragment;
 import com.hufu.weatherman.fragment.MeFragment;
 import com.hufu.weatherman.manager.SystemBarTintManager;
 
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
---
+++ app/src/main/java/com/hufu/weatherman/activity/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.hufu.weatherman.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.hufu.weatherman.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        View view = findViewById(R.id.splash_content);
+        /**
+         * 使用ViewPager+fragment做引导页
+         */
+    }
+
+}
